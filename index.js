836192a26391f500fe85cd2d52af4320afe5be9d
Ahmet Eren Özkılınç <erenster2000@gmail.com>
2022-11-20 02:39:54 +0300

    commit2

--- index.js
+++ index.js
@@ -1,30 +1,84 @@
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+const newspapers = [
+    {
+        name: "sabah",
+        address: "https://www.sabah.com.tr/son-dakika-haberleri",
+        base: "https://www.sabah.com.tr"
+    },
+    {
+        name: "halktv",
+        address: "https://halktv.com.tr/gundem",
+        base: "https://halktv.com.tr"
+    },
+    {
+        name: "tele1",
+        address: "https://tele1.com.tr/son-dakika/",
+        base: ""
+    }
+]
+
 const app = express()
 
 const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address,)
+        .then(response =>{
+            const html = response.data
+            const $ = cheerio.load(html)
+            $('a:contains("İstanbul")',html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url:newspaper.base +url,
+                    source: newspaper.name
+                })
+            })
+
+        } )
+
+})
+
 app.get('/', (req, res) => {
     res.json('Hello, please change containing of "a" in index.js file with your topic to search.')
 })
 
-app.get('/sabah', (req, res) => {
-    axios.get('https://www.sabah.com.tr/gundem')
-        .then((response) =>{
+app.get('/news', (req, res) => {
+
+    res.json(articles)
+
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
+            const specificArticles = []
 
-            $('a:contains("TBMM")',html).each(function () {
+            $('a:contains("İstanbul")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
-                articles.push({
+                specificArticles.push({
                     title,
-                    url
+                    url: newspaperBase + url,
+                    source: newspaperId
                 })
             })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
 })
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
